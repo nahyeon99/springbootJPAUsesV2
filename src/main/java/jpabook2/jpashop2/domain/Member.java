5f7bff5d4cdff65af1b4db5df22f4c33f3615b74
NaHyeon <nahyeonee99@gmail.com>
2022-07-08 01:24:58 +0900

    회원 등록 API

--- src/main/java/jpabook2/jpashop2/domain/Member.java
+++ src/main/java/jpabook2/jpashop2/domain/Member.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,6 +15,8 @@ public class Member {
     @Id @GeneratedValue
     @Column(name="member_id") // 테이블의 column 명 설정
     private Long id;
+
+    @NotEmpty
     private String name;
 
     @Embedded
